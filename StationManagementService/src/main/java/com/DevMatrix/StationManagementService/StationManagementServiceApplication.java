eed4c9f845ed5b339c0ae4309d6b04e24f5912fc
StarAskari <n.askari2k@gmail.com>
2025-06-01 00:20:40 +0200

    creating the config-repo config-server and eureka-server projects

--- StationManagementService/src/main/java/com/DevMatrix/StationManagementService/StationManagementServiceApplication.java
+++ StationManagementService/src/main/java/com/DevMatrix/StationManagementService/StationManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.DevMatrix.StationManagementService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StationManagementServiceApplication {
 
 	public static void main(String[] args) {
