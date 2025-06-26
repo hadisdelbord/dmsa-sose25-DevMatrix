97cd1da05b448c40b0da464c44b483eed4fa3703
Amir Hossein Pakdel <amir.pakdel1378@yahoo.com>
2025-06-26 16:31:41 +0200

    Update Spring Boot version and enable Eureka client in application

--- statistics-service/src/main/java/com/fhdo/statisticsservice/StatisticsServiceApplication.java
+++ statistics-service/src/main/java/com/fhdo/statisticsservice/StatisticsServiceApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableCaching
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class StatisticsServiceApplication {
 
     public static void main(String[] args) {
